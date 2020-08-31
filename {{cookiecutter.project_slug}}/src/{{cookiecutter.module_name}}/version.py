a9fe2732be1184dd0edd9118812ea0ba3c04fa85
Alex McKenzie <hello@alexmck.com>
2020-08-31 15:30:15 +0200

    Fix duplicated version info for poetry

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/version.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/version.py
@@ -1,1 +1,8 @@
-__version__ = '0.1.0'
+{% if cookiecutter.package_manager == 'poetry' %}try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version("{{ cookiecutter.module_name }}")
+{% else %}__version__ = '0.1.0'
+{% endif %}
