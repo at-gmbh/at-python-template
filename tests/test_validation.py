1c5d00c2af969cfc8631f4c7128ee566e4dd1913
Sebastian Straub <sebastian.straub@alexanderthamm.com>
2020-10-07 16:53:34 +0200

    add tests for version checks
    mock incompatible python & cookiecutter versions and see if it fails

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,5 +1,9 @@
+from runpy import run_path
+from typing import Any, Callable, Tuple
+
 import pytest
 from cookiecutter.exceptions import CookiecutterException
+from pytest_mock import MockerFixture
 
 from .util import check_project
 
@@ -17,3 +21,40 @@ def test_project_slug():
 def test_editor():
     check_project(settings={'editor_settings': "hax0r-editor-2000"})
     # note: invalid choices will be silently ignored by cookiecutter
+
+
+def test_fail_python_3_5(mocker: MockerFixture):
+    fail_python_version(mocker, (3, 5, 0))
+
+
+def test_fail_python_2_7(mocker: MockerFixture):
+    fail_python_version(mocker, (2, 7, 0))
+
+
+def test_fail_cookiecutter_1_6_0(mocker: MockerFixture):
+    fail_cookiecutter_version(mocker, "1.6.0")
+
+
+def test_fail_cookiecutter_1_7_1(mocker: MockerFixture):
+    fail_cookiecutter_version(mocker, "1.7.1")
+
+
+def fail_python_version(mocker: MockerFixture, version_info: Tuple[int, int, int]):
+    patch_cookiecutter_run_script(
+        mocker, lambda mocker: mocker.patch('sys.version_info', version_info))
+    with pytest.raises(SystemExit):
+        check_project()
+
+
+def fail_cookiecutter_version(mocker: MockerFixture, version_info: str):
+    patch_cookiecutter_run_script(
+        mocker, lambda mocker: mocker.patch('cookiecutter.__version__', version_info))
+    with pytest.raises(SystemExit):
+        check_project()
+
+
+def patch_cookiecutter_run_script(mocker: MockerFixture, patch_fun: Callable[[MockerFixture], Any]):
+    def run_script_wrapper(script_path, *args, **kwargs):
+        patch_fun(mocker)
+        run_path(script_path)
+    mocker.patch('cookiecutter.hooks.run_script', run_script_wrapper)
