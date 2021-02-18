b4cd9bfb7bbe6f36b7909448266e25c6a94afca4
Maximilian Pensel <maximilian.pensel@alexanderthamm.com>
2021-02-18 10:36:06 +0100

    Add logging example code for when create_cli==no
    Sensitive to logging option (yaml/hocon)
    Add cell in example.ipynb and sample code in main.py

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/main.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/main.py
@@ -1,6 +1,20 @@
+{% if cookiecutter.config_file != 'none' %}{% if cookiecutter.config_file == 'yaml' %}import os
+{% endif %}import logging
+from {{ cookiecutter.module_name }} import util
 
+logger = logging.getLogger("{{ cookiecutter.module_name }}")
+
+{% endif %}
 def main():
-    # TODO your journey starts here
+    {% if cookiecutter.config_file != 'none' %}config = util.load_config({% if cookiecutter.config_file == 'yaml' %}os.path.join(os.path.abspath(__file__),
+                                           *[os.pardir]*3,
+                                           "config",
+                                           "config.yml"){% endif %})
+    util.logging_setup(config)
+
+    logger.info("Looks like you're all set up. Let's get going!")
+
+    {% endif %}# TODO your journey starts here
     print("hello :)")
 
 
