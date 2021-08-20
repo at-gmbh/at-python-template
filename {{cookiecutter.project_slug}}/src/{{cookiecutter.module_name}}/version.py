41cbbed59f78a4de3df0caa66548de12d18f8bc1
Sebastian Straub <sstraub@posteo.de>
2021-08-20 18:39:16 +0200

    whitespace issues fixed

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/version.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/version.py
@@ -4,5 +4,4 @@ except ModuleNotFoundError:
     import importlib_metadata
 
 __version__ = importlib_metadata.version("{{ cookiecutter.module_name }}")
-{% else %}__version__ = '0.1.0'
-{% endif %}
+{%- else %}__version__ = '0.1.0'{% endif %}
