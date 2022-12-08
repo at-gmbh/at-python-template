f125dc26314891fb4f75c837e7a7f288ab2b46f6
Erwin Rudi <erwin.rudi@alexanderthamm.com>
2022-12-08 17:10:34 +0100

    revert black formatting

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -16,43 +16,35 @@ import warnings
 
 import cookiecutter
 
-# check Python version (3.7 or higher)
+# check Python version (3.6 or higher)
 with warnings.catch_warnings():
     warnings.simplefilter("ignore", category=DeprecationWarning)
-    if StrictVersion(platform.python_version()) < StrictVersion("3.7.0"):
-        print(
-            "ERROR: You are using Python {}, but Python 3.7 or higher is required "
-            "to use this template".format(platform.python_version())
-        )
+    if StrictVersion(platform.python_version()) < StrictVersion("3.6.0"):
+        print("ERROR: You are using Python {}, but Python 3.6 or higher is required "
+              "to use this template".format(platform.python_version()))
         sys.exit(1)
-    # check cookiecutter version (1.7.2 or higher)
-    if StrictVersion(cookiecutter.__version__) < StrictVersion("1.7.2"):
-        print(
-            "ERROR: You are using cookiecutter {}, but cookiecutter 1.7.2 or higher is required "
-            "to use this template".format(cookiecutter.__version__)
-        )
+
+      # check cookiecutter version (1.7.2 or higher)
+    if StrictVersion(cookiecutter.__version__) < StrictVersion('1.7.2'):
+        print("ERROR: You are using cookiecutter {}, but cookiecutter 1.7.2 or higher is required "
+              "to use this template".format(cookiecutter.__version__))
         sys.exit(1)
 
 # check the slug and module name
-SLUG_REGEX = r"^[a-zA-Z][-a-zA-Z0-9]+$"
-MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
+SLUG_REGEX = r'^[a-zA-Z][-a-zA-Z0-9]+$'
+MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 
-project_slug = "{{ cookiecutter.project_slug }}"
-module_name = "{{ cookiecutter.module_name }}"
+project_slug = '{{ cookiecutter.project_slug }}'
+module_name = '{{ cookiecutter.module_name }}'
 
 if not re.match(SLUG_REGEX, project_slug):
-    print(
-        "ERROR: {} is not a valid slug! It may only consist of numbers and letters of the "
-        "english alphabet, begin with a letter, and must use dashes instead of whitespace.".format(
-            project_slug
-        )
-    )
+    print("ERROR: {} is not a valid slug! It may only consist of numbers and letters of the "
+          "english alphabet, begin with a letter, and must use dashes instead of whitespace."
+          .format(project_slug))
     sys.exit(1)
 
 if not re.match(MODULE_REGEX, module_name):
-    print(
-        "ERROR: {} is not a valid Python module name! "
-        "See https://www.python.org/dev/peps/pep-0008/#package-and-module-names "
-        "for naming standards.".format(module_name)
-    )
+    print("ERROR: {} is not a valid Python module name! "
+          "See https://www.python.org/dev/peps/pep-0008/#package-and-module-names "
+          "for naming standards.".format(module_name))
     sys.exit(1)
