8450cc9804e9dde9e33c8b68ad4a7bd9bbbd3c5e
Dr. Bert Besser <bert.besser@alexanderthamm.com>
2022-12-19 16:39:29 +0100

    feat: hocon reads env vars

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/main.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/main.py
@@ -1,7 +1,10 @@
+{% if cookiecutter.config_file == 'hocon' %}import os
+from pyhocon import ConfigFactory
 
-def main():
+{% endif %}def main():
     # TODO your journey starts here
-    print("hello :)")
+    {% if cookiecutter.config_file == 'hocon' %}config = ConfigFactory.parse_file(f"config/{os.environ['ENV']}.conf")
+    print(f"hello {config.get('username')} :)"){% else %}print("hello :)"){% endif %}
 
 
 if __name__ == "__main__":
