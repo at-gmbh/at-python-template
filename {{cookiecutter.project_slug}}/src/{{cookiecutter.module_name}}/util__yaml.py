e77f65fb05e378ef9e72b4f8489d78552a55f1e0
Christian Baur <christian.baur@alexanderthamm.com>
2022-12-23 16:35:33 +0100

    #refined pr v2 - deleted whitespaces in functions

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/util__yaml.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/util__yaml.py
@@ -13,11 +13,10 @@ def get_resource_string(path: str, decode=True) -> Union[str, bytes]:
     Load a package resource (i.e. a file from within this package)
 
     :param path: the path, starting at the root of the current module (e.g. 'res/default.conf').
-           must be a string, not a Path object!
+        must be a string, not a Path object!
     :param decode: if true, decode the file contents as string (otherwise return bytes)
     :return: the contents of the resource file (as string or bytes)
     """
-
     s = pkg_resources.resource_string(__name__.split('.')[0], path)
     return s.decode(errors='ignore') if decode else s
 
@@ -29,7 +28,6 @@ def load_config(config_file: Union[str, Path]) -> Dict[str, Any]:
     :param config_file: path of the config file to load
     :return: the parsed config as dictionary
     """
-
     with open(config_file, 'r') as fp:
         return yaml.safe_load(fp)
 
@@ -40,15 +38,11 @@ def logging_setup(config: Dict):
 
     :param config: the parsed config tree
     """
-
     log_conf = config['logging']
     fmt = log_conf['format']
-
     if log_conf['enabled']:
         level = logging._nameToLevel[log_conf['level'].upper()]
     else:
         level = logging.NOTSET
-
     logging.basicConfig(format=fmt, level=logging.WARNING)
-
     logger.setLevel(level)
