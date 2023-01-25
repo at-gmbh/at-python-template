9257b4fb713b56e510b7f6876f9cb9f93abf8de9
Maximilian Pensel <maximilian.pensel@alexanderthamm.com>
2023-01-25 11:54:43 +0100

    Change metadata version reference to project_slug

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/version.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.module_name}}/version.py
@@ -3,5 +3,5 @@
 except ModuleNotFoundError:
     import importlib_metadata
 
-__version__ = importlib_metadata.version("{{ cookiecutter.module_name }}")
+__version__ = importlib_metadata.version("{{ cookiecutter.project_slug }}")
 {%- else %}__version__ = '0.1.0'{% endif %}
