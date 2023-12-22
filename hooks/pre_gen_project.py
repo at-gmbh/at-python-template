3bbc7379bc836ea16de521841050afffbba97b38
Sebastian Straub <sstraub@posteo.de>
2023-12-22 16:11:12 +0100

    indentation

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -27,7 +27,7 @@ with warnings.catch_warnings():
     # check cookiecutter version (1.7.2 or higher)
     if StrictVersion(cookiecutter.__version__) < StrictVersion('1.7.2'):
         print(f"ERROR: You are using cookiecutter {cookiecutter.__version__}" 
-            "but cookiecutter 1.7.2 or higher is required to use this template")
+              "but cookiecutter 1.7.2 or higher is required to use this template")
         sys.exit(1)
 
 # check the slug and module name
